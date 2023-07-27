59c0253331b5aa6aed89b9976109e01fc0f736cb
yogiMaan <133136369+yogiMaan@users.noreply.github.com>
2023-07-27 12:40:27 +0100

    basic error handling

--- stoxx_capping_service/capping_Core.py
+++ stoxx_capping_service/capping_Core.py
@@ -8,6 +8,8 @@ from core_logger import get_logger
 
 logger = get_logger(__name__, 'logs/debug.log', use_formatter=True)
 df_logger = get_logger(str(__name__) + '_dfs', 'logs/debug.log', use_formatter=False)
+
+
 def cap_component(
         df_grouped: pd.DataFrame,
         cap_limit: float,
@@ -16,109 +18,118 @@ def cap_component(
         sum_truncated_weight_under_cap_limit: float = None,
         exclude_nth_rows_from_cap: int = None,
 ):
-    logger.info("Function %s:", sys._getframe().f_code.co_name)
-    if is_first_iteration and exclude_nth_rows_from_cap is None:
-        # the rounding line stops a testcase where sum = 1.0000000000000002 from failing
-        component_sum = round(df_grouped["mcap"].sum(), 15)
-        df_grouped["orig_pct"] = df_grouped.apply(
-            lambda x: x["mcap"] / component_sum, axis=1
+    try:
+        logger.info("Function %s:", sys._getframe().f_code.co_name)
+        if is_first_iteration and exclude_nth_rows_from_cap is None:
+            # the rounding line stops a testcase where sum = 1.0000000000000002 from failing
+            component_sum = round(df_grouped["mcap"].sum(), 15)
+            df_grouped["orig_pct"] = df_grouped.apply(
+                lambda x: x["mcap"] / component_sum, axis=1
+            )
+            df_grouped["pct"] = df_grouped["orig_pct"]
+        else:
+            df_grouped.drop(
+                columns=[
+                    "apply_cap",
+                    "residuals",
+                    "truncated_weight",
+                    "underweight_pct",
+                    "spread_residual",
+                    "weight_plus_residual",
+                    "factor",
+                ],
+                inplace=True,
+            )
+
+        df_grouped["apply_cap"] = True
+
+        if exclude_nth_rows_from_cap is not None:
+            # Update the column values for the first n rows
+            df_grouped.loc[
+                df_grouped.index[:exclude_nth_rows_from_cap], "apply_cap"
+            ] = False
+
+        df_grouped["residuals"] = df_grouped.apply(
+            lambda x: x["pct"] - cap_limit
+            if x["pct"] > cap_limit and x["apply_cap"] == True
+            else 0,
+            axis=1,
         )
-        df_grouped["pct"] = df_grouped["orig_pct"]
-    else:
-        df_grouped.drop(
-            columns=[
-                "apply_cap",
-                "residuals",
-                "truncated_weight",
-                "underweight_pct",
-                "spread_residual",
-                "weight_plus_residual",
-                "factor",
-            ],
-            inplace=True,
-        )
+        if sum_residuals is None:
+            sum_residuals = round(df_grouped["residuals"].sum(), 15)
 
-    df_grouped["apply_cap"] = True
-
-    if exclude_nth_rows_from_cap is not None:
-        # Update the column values for the first n rows
-        df_grouped.loc[
-            df_grouped.index[:exclude_nth_rows_from_cap], "apply_cap"
-        ] = False
-
-    df_grouped["residuals"] = df_grouped.apply(
-        lambda x: x["pct"] - cap_limit
-        if x["pct"] > cap_limit and x["apply_cap"] == True
-        else 0,
-        axis=1,
-    )
-    if sum_residuals is None:
-        sum_residuals = round(df_grouped["residuals"].sum(), 15)
-
-    df_grouped["truncated_weight"] = df_grouped.apply(
-        lambda x: x["pct"] - x["residuals"], axis=1
-    )
-
-    if sum_truncated_weight_under_cap_limit is None:
-        sum_truncated_weight_under_cap_limit = round(
-            df_grouped[df_grouped["truncated_weight"] < cap_limit][
-                "truncated_weight"
-            ].sum(),
-            15,
+        df_grouped["truncated_weight"] = df_grouped.apply(
+            lambda x: x["pct"] - x["residuals"], axis=1
         )
 
-    df_grouped["underweight_pct"] = df_grouped.apply(
-        lambda x: x["truncated_weight"] / sum_truncated_weight_under_cap_limit
-        if x["truncated_weight"] < cap_limit
-        else 0,
-        axis=1,
-    )
+        if sum_truncated_weight_under_cap_limit is None:
+            sum_truncated_weight_under_cap_limit = round(
+                df_grouped[df_grouped["truncated_weight"] < cap_limit][
+                    "truncated_weight"
+                ].sum(),
+                15,
+            )
 
-    df_grouped["spread_residual"] = df_grouped.apply(
-        lambda x: x["underweight_pct"] * sum_residuals, axis=1
-    )
-
-    df_grouped["weight_plus_residual"] = df_grouped.apply(
-        lambda x: x["truncated_weight"] + x["spread_residual"], axis=1
-    )
-
-    df_grouped["weight_plus_residual"] = df_grouped.apply(
-        lambda x: x["truncated_weight"] + x["spread_residual"], axis=1
-    )
-    df_grouped["factor"] = df_grouped.apply(
-        lambda x: x["weight_plus_residual"] / x["orig_pct"],
-        axis=1,
-    )
-
-    any_overweight_component = (
-            (df_grouped["weight_plus_residual"] > cap_limit) & df_grouped["apply_cap"]
-    ).any()
-
-    df_grouped["pct"] = df_grouped["weight_plus_residual"]
-
-    if any_overweight_component:
-        return cap_component(
-            df_grouped=df_grouped,
-            cap_limit=cap_limit,
-            is_first_iteration=False,
-            sum_residuals=sum_residuals,
-            sum_truncated_weight_under_cap_limit=sum_truncated_weight_under_cap_limit,
-            exclude_nth_rows_from_cap=exclude_nth_rows_from_cap,
+        df_grouped["underweight_pct"] = df_grouped.apply(
+            lambda x: x["truncated_weight"] / sum_truncated_weight_under_cap_limit
+            if x["truncated_weight"] < cap_limit
+            else 0,
+            axis=1,
         )
-    else:
-        return df_grouped
+
+        df_grouped["spread_residual"] = df_grouped.apply(
+            lambda x: x["underweight_pct"] * sum_residuals, axis=1
+        )
+
+        df_grouped["weight_plus_residual"] = df_grouped.apply(
+            lambda x: x["truncated_weight"] + x["spread_residual"], axis=1
+        )
+
+        df_grouped["weight_plus_residual"] = df_grouped.apply(
+            lambda x: x["truncated_weight"] + x["spread_residual"], axis=1
+        )
+        df_grouped["factor"] = df_grouped.apply(
+            lambda x: x["weight_plus_residual"] / x["orig_pct"],
+            axis=1,
+        )
+
+        any_overweight_component = (
+                (df_grouped["weight_plus_residual"] > cap_limit) & df_grouped["apply_cap"]
+        ).any()
+
+        df_grouped["pct"] = df_grouped["weight_plus_residual"]
+
+        if any_overweight_component:
+            return cap_component(
+                df_grouped=df_grouped,
+                cap_limit=cap_limit,
+                is_first_iteration=False,
+                sum_residuals=sum_residuals,
+                sum_truncated_weight_under_cap_limit=sum_truncated_weight_under_cap_limit,
+                exclude_nth_rows_from_cap=exclude_nth_rows_from_cap,
+            )
+        else:
+            return df_grouped
+    except Exception as e:
+        logger.error("Error in %s: %s", sys._getframe().f_code.co_name, e)
+        raise e
 
 
 def __after_nth_largest_components(componentPcts, n):
-    logger.info("Function %s: starting", sys._getframe().f_code.co_name)
-    excludes = set()
-    if n <= 1:
+    try:
+        logger.info("Function %s: starting", sys._getframe().f_code.co_name)
+        excludes = set()
+        if n <= 1:
+            return excludes
+        componentPctsSortedDesc = sorted(componentPcts.items(), key=lambda item: item[1], reverse=True)
+        i = 0
+        while (i < (n - 1) and i < len(componentPctsSortedDesc)):
+            excludes.add(componentPctsSortedDesc[i][0])  # just get the key, not the key/value pair
+            i += 1
+            logger.info("excludes: %s", excludes)
+            logger.info("Function %s: Finished", sys._getframe().f_code.co_name)
+    except Exception as e:
+        logger.error("Error in %s: %s", sys._getframe().f_code.co_name, e)
+        raise e
+    else:
         return excludes
-    componentPctsSortedDesc = sorted(componentPcts.items(), key=lambda item: item[1], reverse=True)
-    i = 0
-    while (i < (n - 1) and i < len(componentPctsSortedDesc)):
-        excludes.add(componentPctsSortedDesc[i][0])  # just get the key, not the key/value pair
-        i += 1
-        logger.info("excludes: %s", excludes)
-        logger.info("Function %s: Finished", sys._getframe().f_code.co_name)
-    return excludes
--- stoxx_capping_service/capping_server.py
+++ stoxx_capping_service/capping_server.py
@@ -21,139 +21,155 @@ class CappingServicer(capping_pb2_grpc.CappingServicer):
 
     @staticmethod
     def __mcaps_to_data_frame(mcaps):
-        rows = list()
-        for mcap in mcaps:
-            d = {"mcap": mcap.mcap, "c1": mcap.components[0], "ConstituentId": mcap.ConstituentId}
+        try:
+            rows = list()
+            for mcap in mcaps:
+                d = {"mcap": mcap.mcap, "c1": mcap.components[0], "ConstituentId": mcap.ConstituentId}
 
-            # if we are doing multi component capping, add the additional components
-            for i in range(1, len(mcap.components)):
-                d["c" + str(i + 1)] = mcap.components[i]
-            rows.append(d)
-
-        return pd.DataFrame(rows)
+                # if we are doing multi component capping, add the additional components
+                for i in range(1, len(mcap.components)):
+                    d["c" + str(i + 1)] = mcap.components[i]
+                rows.append(d)
+        except Exception as e:
+            logger.error("Error in function %s: %s", sys._getframe().f_code.co_name, e)
+            raise e
+        else:
+            return pd.DataFrame(rows)
 
     @staticmethod
     def __multiply_factors(num_components: int, df_row: pd.Series):
-        factor = df_row["c1_factor"] if "c1_factor" in df_row else df_row["factor"]
+        try:
+            factor = df_row["c1_factor"] if "c1_factor" in df_row else df_row["factor"]
 
-        if num_components == 1:
+            if num_components == 1:
+                return factor
+
+            for i in range(2, (num_components + 1)):
+                factor *= df_row["c" + str(i) + "_factor"]
+        except Exception as e:
+            logger.error("Error in function %s: %s", sys._getframe().f_code.co_name, e)
+            raise e
+        else:
             return factor
 
-        for i in range(2, (num_components + 1)):
-            factor *= df_row["c" + str(i) + "_factor"]
-
-        return factor
-
     def __cap(self, request):
-        logger.info("Function %s:", sys._getframe().f_code.co_name)
-        df_mcaps = self.__mcaps_to_data_frame(mcaps=request.mcaps)
+        try:
+            logger.info("Function %s:", sys._getframe().f_code.co_name)
+            df_mcaps = self.__mcaps_to_data_frame(mcaps=request.mcaps)
 
-        for i_component_index, methodology_data in enumerate(request.methodologyDatas):
-            logger.info("methodology: %s", capping_pb2.Methodology.Name(methodology_data.methodology))
-            logger.info("Running Iteration %s: ", str(i_component_index + 1))
+            for i_component_index, methodology_data in enumerate(request.methodologyDatas):
+                logger.info("methodology: %s", capping_pb2.Methodology.Name(methodology_data.methodology))
+                logger.info("Running Iteration %s: ", str(i_component_index + 1))
 
-            if methodology_data.methodology == capping_pb2.Methodology_Exposure:
-                df_parent_mcaps = self.__mcaps_to_data_frame(mcaps=request.parent_mcaps)
-                df_final = exposure.cap_exposure(
-                    df_parent_mcaps=df_parent_mcaps, df_mcaps=df_mcaps
-                )
-                continue
+                if methodology_data.methodology == capping_pb2.Methodology_Exposure:
+                    df_parent_mcaps = self.__mcaps_to_data_frame(mcaps=request.parent_mcaps)
+                    df_final = exposure.cap_exposure(
+                        df_parent_mcaps=df_parent_mcaps, df_mcaps=df_mcaps
+                    )
+                    continue
 
-            if i_component_index > 0:
-                df_merged = df_mcaps.merge(df_grouped, on="c1", how="inner")[
-                    ["c1", "c2", "weight_plus_residual", "ConstituentId"]
-                ]
-                df_merged.rename(columns={"weight_plus_residual": "mcap"}, inplace=True)
-                df_merged.drop_duplicates(
-                    subset="c" + str(i_component_index), inplace=True
+                if i_component_index > 0:
+                    df_merged = df_mcaps.merge(df_grouped, on="c1", how="inner")[
+                        ["c1", "c2", "weight_plus_residual", "ConstituentId"]
+                    ]
+                    df_merged.rename(columns={"weight_plus_residual": "mcap"}, inplace=True)
+                    df_merged.drop_duplicates(
+                        subset="c" + str(i_component_index), inplace=True
+                    )
+                    df_grouped = (
+                        df_merged.groupby("c" + str(i_component_index + 1))["mcap"]
+                        .sum()
+                        .reset_index()
+                        .sort_values("mcap", ascending=False)
+                    )
+
+                else:
+                    df_grouped = (
+                        df_mcaps.groupby("c" + str(i_component_index + 1))["mcap"]
+                        .sum()
+                        .reset_index()
+                        .sort_values("mcap", ascending=False)
+                    )
+                logger.info("Iteration %s Grouped Mcaps : ", str(i_component_index + 1))
+                df_logger.info("\n %s", df_mcaps.to_markdown(index=False))
+                if len(methodology_data.limitInfos) == 0:
+                    cap_limit = 0
+                else:
+                    limit_info = methodology_data.limitInfos[0]
+                    cap_limit = limit_info.limit
+                logger.info("Iteration %s Limit %s: ",str(i_component_index + 1), cap_limit)
+                if methodology_data.methodology == capping_pb2.Methodology_Ladder:
+                    df_grouped = ladder.cap_ladder(
+                        methodology_data=methodology_data,
+                        df_grouped=df_grouped
+                    )
+                else:
+                    df_grouped = Core.cap_component(
+                        df_grouped=df_grouped,
+                        cap_limit=cap_limit,
+                        is_first_iteration=True,
+                    )
+
+                cols = list(
+                    df_mcaps.columns if i_component_index == 0 else df_final.columns
                 )
-                df_grouped = (
-                    df_merged.groupby("c" + str(i_component_index + 1))["mcap"]
-                    .sum()
-                    .reset_index()
-                    .sort_values("mcap", ascending=False)
+                cols.append("factor")
+                cols[0] = "mcap_x"
+
+                if i_component_index == 0:
+                    df_final = df_mcaps.merge(
+                        df_grouped, on="c" + str(i_component_index + 1), how="inner"
+                    )[cols]
+                else:
+                    df_final = df_final.merge(
+                        df_grouped, on="c" + str(i_component_index + 1), how="inner"
+                    )[cols]
+
+                df_final.rename(
+                    columns={
+                        "mcap_x": "mcap",
+                        "factor": "c" + str(i_component_index + 1) + "_factor",
+                    },
+                    inplace=True,
                 )
 
-            else:
-                df_grouped = (
-                    df_mcaps.groupby("c" + str(i_component_index + 1))["mcap"]
-                    .sum()
-                    .reset_index()
-                    .sort_values("mcap", ascending=False)
-                )
-            logger.info("Iteration %s Grouped Mcaps : ", str(i_component_index + 1))
-            df_logger.info("\n %s", df_mcaps.to_markdown(index=False))
-            if len(methodology_data.limitInfos) == 0:
-                cap_limit = 0
-            else:
-                limit_info = methodology_data.limitInfos[0]
-                cap_limit = limit_info.limit
-            logger.info("Iteration %s Limit %s: ",str(i_component_index + 1), cap_limit)
-            if methodology_data.methodology == capping_pb2.Methodology_Ladder:
-                df_grouped = ladder.cap_ladder(
-                    methodology_data=methodology_data,
-                    df_grouped=df_grouped
-                )
-            else:
-                df_grouped = Core.cap_component(
-                    df_grouped=df_grouped,
-                    cap_limit=cap_limit,
-                    is_first_iteration=True,
-                )
-
-            cols = list(
-                df_mcaps.columns if i_component_index == 0 else df_final.columns
+            df_final["factor"] = df_final.apply(
+                lambda x: self.__multiply_factors(
+                    num_components=(i_component_index + 1), df_row=x
+                ),
+                axis=1,
             )
-            cols.append("factor")
-            cols[0] = "mcap_x"
-
-            if i_component_index == 0:
-                df_final = df_mcaps.merge(
-                    df_grouped, on="c" + str(i_component_index + 1), how="inner"
-                )[cols]
-            else:
-                df_final = df_final.merge(
-                    df_grouped, on="c" + str(i_component_index + 1), how="inner"
-                )[cols]
-
-            df_final.rename(
-                columns={
-                    "mcap_x": "mcap",
-                    "factor": "c" + str(i_component_index + 1) + "_factor",
-                },
-                inplace=True,
-            )
-
-        df_final["factor"] = df_final.apply(
-            lambda x: self.__multiply_factors(
-                num_components=(i_component_index + 1), df_row=x
-            ),
-            axis=1,
-        )
-        logger.info("result after Iteration %s : ", str(i_component_index + 1))
-        df_logger.info("\n %s", df_final.to_markdown(index=False))
-        return df_final
+            logger.info("result after Iteration %s : ", str(i_component_index + 1))
+            df_logger.info("\n %s", df_final.to_markdown(index=False))
+        except Exception as e:
+            logger.error("Error in %s: %s", sys._getframe().f_code.co_name, e)
+            raise e
+        else:
+            return df_final
 
     def Cap(self, request, context):
+        try:
+            logger.info("Function %s:", sys._getframe().f_code.co_name)
+            df_factors = self.__cap(request=request)
 
-        logger.info("Function %s:", sys._getframe().f_code.co_name)
-        df_factors = self.__cap(request=request)
+            cap_result = capping_pb2.CapResult()
+            factors = list(df_factors["factor"])
+            max_factor = max(factors)
 
-        cap_result = capping_pb2.CapResult()
-        factors = list(df_factors["factor"])
-        max_factor = max(factors)
+            for index, row in df_factors.iterrows():
+                if request.mcapDecreasingFactors:
+                    factor = capping_pb2.Capfactor(ConstituentID=row["ConstituentId"],
+                                                   factor=round(row["factor"] / max_factor, 15))
+                    cap_result.capfactors.append(factor)
 
-        for index, row in df_factors.iterrows():
-            if request.mcapDecreasingFactors:
-                factor = capping_pb2.Capfactor(ConstituentID=row["ConstituentId"],
-                                               factor=round(row["factor"] / max_factor, 15))
-                cap_result.capfactors.append(factor)
-
-            else:
-                factor = capping_pb2.Capfactor(ConstituentID=row["ConstituentId"], factor=round(row["factor"], 15))
-                cap_result.capfactors.append(factor)
-
-        return cap_result
+                else:
+                    factor = capping_pb2.Capfactor(ConstituentID=row["ConstituentId"], factor=round(row["factor"], 15))
+                    cap_result.capfactors.append(factor)
+        except Exception as e:
+            logger.error("Error in Cap: %s", e)
+            raise e
+        else:
+            return cap_result
 
 
 def serve():
